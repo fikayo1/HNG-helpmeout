201979f826e806e5caf10de194776515a721379a
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-10-02 11:48:12 +0100

    Updated code to allow for chunk data

--- chromeapp/settings.py
+++ chromeapp/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'upload',
-    
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- chromeapp/urls.py
+++ chromeapp/urls.py
@@ -17,7 +17,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Helpmeout APIs",
+        default_version="v1",
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    
     path('admin/', admin.site.urls),
     path('api/', include('upload.urls')),
 ]
